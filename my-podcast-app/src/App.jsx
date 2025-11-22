a66fa083c99b9ba9bf889a76cd71cabcb55296f8
Nontuthuzelo Mtolo <217491599+NontuthuzeloM24@users.noreply.github.com>
2025-11-22 09:49:44 +0200

    update the imports in App.jsx

--- my-podcast-app/src/App.jsx
+++ my-podcast-app/src/App.jsx
@@ -1,5 +1,5 @@
-// src/App.jsx
 import React from "react";
+import styles from "./App.css"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { PodcastContextProvider } from "./context/PodcastContext";
 import Header from "./components/UI/Header";
